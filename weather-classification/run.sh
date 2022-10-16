a75a8aba0dfef8bce1ddbdd46f0769e2c9099735
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-10-16 09:55:42 -0400

    Initial commit.

---
+++ weather-classification/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+
+
+count=1
+while [ -d "save${count}" ]
+do
+  let "count++"
+done
+mkdir "save${count}"
+
+
+declare -a arr=("courant" "dijkstra" "galois" "hardy" "noyce" "nygaard" "perlis" "shannon" "ulam" "wilkinson" "zeno" "zorn" "ada" "alonzo" "babbage" "borg" "cantor" "cray" "dahl" "eckert" "erdos" "euclid" "euler" "fermat" "fourier" "frege" "gauss" "godel" "grace" "hamming" "hilbert" "noether" "pascal" "peano" "riemann" "wilkes" "zermelo")
+
+for i in "${arr[@]}"
+do
+	ssh "$i" "cd ~/Documents/GitHub/SemanticSegmentationOfLowVisibilityRoadways/weather-classification/; python3 research.py ~/Documents/weather_dataset/resized/ save${count}/ > /dev/null 2>&1 & exit"
+done
