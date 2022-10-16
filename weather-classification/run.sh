6c94710e10b5cab41ad12299b443ad98b4c811a1
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-10-16 10:05:05 -0400

    Changed host key.

--- weather-classification/run.sh
+++ weather-classification/run.sh
@@ -10,9 +10,10 @@ done
 mkdir "save${count}"
 
 
-declare -a arr=("courant" "dijkstra" "galois" "hardy" "noyce" "nygaard" "perlis" "shannon" "ulam" "wilkinson" "zeno" "zorn" "ada" "alonzo" "babbage" "borg" "cantor" "cray" "dahl" "eckert" "erdos" "euclid" "euler" "fermat" "fourier" "frege" "gauss" "godel" "grace" "hamming" "hilbert" "noether" "pascal" "peano" "riemann" "wilkes" "zermelo")
+#declare -a arr=("courant" "dijkstra" "galois" "hardy" "noyce" "nygaard" "perlis" "shannon" "ulam" "wilkinson" "zeno" "zorn" "ada" "alonzo" "babbage" "borg" "cantor" "cray" "dahl" "eckert" "erdos" "euclid" "euler" "fermat" "fourier" "frege" "gauss" "godel" "grace" "hamming" "hilbert" "noether" "pascal" "peano" "riemann" "wilkes" "zermelo")
+declare -a arr=("courant" "zermelo")
 
 for i in "${arr[@]}"
 do
-	ssh "$i" "cd ~/Documents/GitHub/SemanticSegmentationOfLowVisibilityRoadways/weather-classification/; python3 research.py ~/Documents/weather_dataset/resized/ save${count}/ > /dev/null 2>&1 & exit"
+	ssh "${i}.hbg.psu.edu" "cd ~/Documents/GitHub/SemanticSegmentationOfLowVisibilityRoadways/weather-classification/; python3 research.py ~/Documents/weather_dataset/resized/ save${count}/ > /dev/null 2>&1 & exit"
 done
