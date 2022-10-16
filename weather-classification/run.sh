5ab5072feb03de4ac3d409fd4fbd78684122c17a
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-10-16 10:06:23 -0400

    Added host names.

--- weather-classification/run.sh
+++ weather-classification/run.sh
@@ -10,8 +10,7 @@ done
 mkdir "save${count}"
 
 
-#declare -a arr=("courant" "dijkstra" "galois" "hardy" "noyce" "nygaard" "perlis" "shannon" "ulam" "wilkinson" "zeno" "zorn" "ada" "alonzo" "babbage" "borg" "cantor" "cray" "dahl" "eckert" "erdos" "euclid" "euler" "fermat" "fourier" "frege" "gauss" "godel" "grace" "hamming" "hilbert" "noether" "pascal" "peano" "riemann" "wilkes" "zermelo")
-declare -a arr=("courant" "zermelo")
+declare -a arr=("courant" "dijkstra" "galois" "hardy" "noyce" "nygaard" "perlis" "shannon" "ulam" "wilkinson" "zeno" "zorn" "ada" "alonzo" "babbage" "borg" "cantor" "cray" "dahl" "eckert" "erdos" "euclid" "euler" "fermat" "fourier" "frege" "gauss" "godel" "grace" "hamming" "hilbert" "noether" "pascal" "peano" "riemann" "wilkes" "zermelo")
 
 for i in "${arr[@]}"
 do
